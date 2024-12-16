d69f88f62f79686e6027d1a1eb5029bd897d2f1b
AgatheshPR <126443738+AgatheshPR@users.noreply.github.com>
2024-12-16 19:49:27 +0530

    Update DButil.java

--- LibraryManagementSystem/src/com/kce/util/DButil.java
+++ LibraryManagementSystem/src/com/kce/util/DButil.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DButil {
 	public static Connection getConnection() throws SQLException {
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/LibManagement","root","Anu27@#");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/LibManagement","root","A27@#");
 		return con;
 	}
 }
